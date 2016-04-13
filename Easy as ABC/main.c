7494b46aa2b7240af88dc72ae18830b7890c904a
Christian Michel <christianmichel107@gmail.com>
2016-04-13 09:40:22 -0400

    Initial Commit

---
+++ Easy as ABC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Easy as ABC
+//
+//  Created by Christian Michel on 4/13/16.
+//  Copyright © 2016 Christian Michel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
